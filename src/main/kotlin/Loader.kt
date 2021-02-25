258d198e5ad3e427dff572e3c75ae70a1a63b6e3
breandan <bre@ndan.co>
2021-02-25 02:10:10 -0500

    use suffix tree

--- src/main/kotlin/Loader.kt
+++ src/main/kotlin/Loader.kt
@@ -1,8 +1,8 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.*
 import com.google.common.jimfs.*
-import com.googlecode.concurrenttrees.radix.ConcurrentRadixTree
 import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharArrayNodeFactory
+import com.googlecode.concurrenttrees.suffix.ConcurrentSuffixTree
 import java.nio.file.*
 import java.util.*
 import java.util.concurrent.ConcurrentLinkedQueue
@@ -22,18 +22,19 @@ class Loader: CliktCommand() {
   // An in-memory file system mirroring the contents of rootDir
   val jfs = Jimfs.newFileSystem(Configuration.forCurrentPlatform())
   // Radix trie multimap for (file, offset) pairs of matching prefixes
-  val trie = ConcurrentRadixTree<Queue<Pair<Path, Int>>>(DefaultCharArrayNodeFactory())
+  val trie = ConcurrentSuffixTree<Queue<Pair<Path, Int>>>(DefaultCharArrayNodeFactory())
 
   override fun run() {
     println("Searching $rootDir for $query")
     val jfsRoot = Path.of(rootDir).mirrorHDFS()
-    indexFS(jfsRoot)
+    measureTimeMillis { indexFS(jfsRoot) }
+      .let { println("Indexing took $it ms") }
 
     measureTimeMillis { jfsRoot.grep(query)
       .also { println("Grep found ${it.size} results") } }
       .let { println("Grep took $it ms") }
 
-    measureTimeMillis { trie.getValuesForKeysStartingWith(query)
+    measureTimeMillis { trie.getValuesForKeysContaining(query)
       .also { println("Trie found ${ it.flatten().size } results") } }
       .let { println("Trie took $it ms") }
   }
