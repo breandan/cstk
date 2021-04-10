724a0abf265de37164e647dfd62cd108bc91c0b4
breandan <bre@ndan.co>
2021-04-10 16:15:10 -0400

    annotate common substrings in each neighborhood

--- src/main/kotlin/edu/mcgill/gymfs/experiments/SimilarFragments.kt
+++ src/main/kotlin/edu/mcgill/gymfs/experiments/SimilarFragments.kt
@@ -1,5 +1,8 @@
 package edu.mcgill.gymfs.experiments
 
+import com.github.jelmerk.knn.SearchResult
+import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharSequenceNodeFactory
+import com.googlecode.concurrenttrees.solver.LCSubstringSolver
 import edu.mcgill.gymfs.disk.*
 import java.io.File
 
@@ -17,21 +20,47 @@ fun main() {
     val knn = knnIndex.findNearest(v, topK)
       .filter { it.item().toString() != l }
       .distinctBy { it.item().toString() }
-      .take(10)
-    Triple(l, knn, knn.sumByDouble { it.distance() })
-  }.sortedBy { (_, _, totalDistance) -> totalDistance }
+      .take(20)
+    Neighborhood(l, knn)
+  }.sortedBy { it.totalDistance }
 
-  println("Nearest nearest neighbors by cumulative similarity\n")
+  println("Nearest nearest neighbors by cumulative similarity")
+  println("Angle brackets enclose longest common substring up to current result\n")
 
   mostSimilar.distinct()
-    .filter { "import" !in it.first }
-    .take(10)
-    .forEachIndexed { i, (label, knn, totalDistance) ->
-      println("$i.] $label")
-      knn.forEachIndexed { j, it ->
-        println("\t$i.$j.] " + it.item().toString())
+    .filter { "import" !in it.origin }
+    .take(20)
+    .forEachIndexed { i, neighborhood ->
+      println("$i.] ${neighborhood.origin}")
+      neighborhood.prettyPrinted.forEachIndexed { j, it ->
+        println("\t$i.$j] $it")
       }
       println()
       println()
     }
+}
+
+data class Neighborhood(
+  val origin: String,
+  val nearestNeighbors: List<SearchResult<Fragment, Double>>,
+) {
+  val totalDistance by lazy { nearestNeighbors.sumByDouble { it.distance() } }
+
+  val longestCommonSubstringSoFar by lazy {
+    nearestNeighbors.mapIndexed { i, _ ->
+      nearestNeighbors.subList(0, i + 1).map { it.item().toString() }
+    }.map { allResultsUpToCurrent ->
+      if (allResultsUpToCurrent.size < 2) ""
+      else LCSubstringSolver(DefaultCharSequenceNodeFactory())
+        .apply { allResultsUpToCurrent.forEach { add(it) } }
+        .longestCommonSubstring.toString()
+    }
+  }
+
+  val prettyPrinted by lazy {
+    nearestNeighbors.zip(longestCommonSubstringSoFar).map { (result, substring) ->
+      if(substring.length < 2) result.item().toString()
+      else result.item().toString().replace(substring, "《$substring》")
+    }
+  }
 }
