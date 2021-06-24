eaa62f02912e0fa03582b8e5205323eb846cfa0c
breandan.considine <breandan.considine@gmail.com>
2021-06-23 23:32:20 -0400

    compute pearson correlation for each singleton dimension

--- src/main/kotlin/edu/mcgill/gymfs/experiments/DimensionAnalysis.kt
+++ src/main/kotlin/edu/mcgill/gymfs/experiments/DimensionAnalysis.kt
@@ -1,22 +1,20 @@
 package edu.mcgill.gymfs.experiments
 
-import edu.mcgill.gymfs.disk.*
+import edu.mcgill.gymfs.disk.MetricCSNF
 import edu.mcgill.gymfs.math.*
 import edu.mcgill.markovian.pmap
-import info.debatty.java.stringsimilarity.*
 import info.debatty.java.stringsimilarity.interfaces.MetricStringDistance
-import kotlin.math.*
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation
 
 // Do certain dimensions correlate more strongly with string edit distance?
 fun main() {
   val data = fetchOrLoadSampleData().let { (l, v) -> l.zip(v) }.take(100)
-  //compare correlation between string metrics and learned metric along dimensions
-  println("dim, correlation")
+  // Compare correlation between string metrics and learned metric along individual dimensions
+  println("dim,correlation")
   (0 until data.first().second.size - 1).pmap { i ->
-    println(i)
     compareDistanceMetricsOnDim(data, (i..i + 1).toList())
   }.sortedBy { it.second }
-    .forEach { println("${it.first.first()} ${it.second}") }
+    .forEach { println("${it.first.first()},${it.second}") }
 }
 
 private fun compareDistanceMetricsOnDim(
@@ -25,8 +23,8 @@ private fun compareDistanceMetricsOnDim(
   stringMetric: MetricStringDistance = MetricCSNF,
 ): Pair<Collection<Int>, Double> = cartProd(data, data)
   .map { (s1, s2) ->
-    (stringMetric.distance(s1.first, s2.first) * 100).toInt() to
-      euclidDist(s1.second.sliceArray(dims), s2.second)
-  }.groupBy(Pair<Int, Double>::first)
-  .mapValues { it.value.map { (_, euclid) -> euclid }.variance() }
-  .values.sum().let { dims to it }
+    stringMetric.distance(s1.first, s2.first) to
+      euclidDist(s1.second.sliceArray(dims), s2.second.sliceArray(dims))
+  } .map { (a, b) -> a to b }.unzip().let { (a, b) ->
+    dims to PearsonsCorrelation().correlation(a.toDoubleArray(), b.toDoubleArray())
+  }
