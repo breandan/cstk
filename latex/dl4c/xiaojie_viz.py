fbcdc21f8d89526cd742c1b3d813e643c3e45eb7
breandan <bre@ndan.co>
2022-02-20 04:21:05 -0500

    rewrite and refactor latex

--- latex/dl4c/xiaojie_viz.py
+++ latex/dl4c/xiaojie_viz.py
@@ -1,10 +1,11 @@
-import json
 import os
 import numpy as np
 import seaborn
 import matplotlib.pyplot as plt
+import json
+import pandas as pd
 
-############### Parameters for graphs ################
+############## Parameters for graphs by hist_visual ################
 # Define the colors and the corresponding value intervals for each color
 # e.g. value < 0.2 -> color = lightsteelblue
 #      0.2 <= value < 0.4 -> color = cornflowerblue
@@ -12,49 +13,97 @@ import matplotlib.pyplot as plt
 # Make sure that len(COLORS) = len(VALUE_INTERVALS) + 1
 # For more colors, please check https://matplotlib.org/stable/gallery/color/named_colors.html
 
-COLORS = ['lightsteelblue', 'cornflowerblue', 'royalblue', 'blue']
-VALUE_INTERVALS = [0.2, 0.4, 0.6]
+COLORS_BEFORE = ['mistyrose', 'lightcoral', 'red', 'darkred']
+COLORS_AFTER = ['lightsteelblue', 'royalblue', 'blue', 'navy']
+VALUE_INTERVALS = [0.25, 0.5, 0.75]
+BINWIDTH = 0.1
+BINS = np.arange(0.0, 1+BINWIDTH, BINWIDTH)
 
-BINWIDTH = 0.02
+
 ######################################################
 
-
-def visualization(data, model_name, sct, img_dir):
+def hist_visual(data: dict, model_name, sct, img_dir):
+    # data is in the form of data = {'before': [], 'after':[]}
 
     # compute mean and variance
-    data = np.array(data)
-    mean = np.mean(data)
-    var = np.var(data)
+    data_after = np.array(data['after'])
+    mean = np.mean(data_after)
+    var = np.var(data_after)
 
     # decide the graph color based on mean and var values, i.e. deeper colors for mean with larger values
-    color = COLORS[0]
+    color_ind = 0
     for i in range(len(VALUE_INTERVALS)):
         if VALUE_INTERVALS[i] <= mean:
-            color = COLORS[i + 1]
+            color_ind = i + 1
         else:
             break
 
-    plt.figure()
-    sns_plot = seaborn.histplot(data=data, color=color, binwidth=BINWIDTH)\
-        .set_title('{} x {}\n mean:{:.3f}, var:{:.3f}'.format(model_name, sct, mean, var))
+    plt.figure(figsize=(10, 9))
+    seaborn.set_theme(style="whitegrid")
+    plt.hist([data['before'], data['after']], color=[COLORS_BEFORE[color_ind], COLORS_AFTER[color_ind]],
+             label=['before', 'after'], alpha=0.9, bins=BINS)
+    plt.xticks(BINS)
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0)
+    plt.title(f"{model_name} x {sct}", fontsize=20)
+
+    # Save image
+    filename = model_name.replace('/', '_')
+    plt.savefig(os.path.join(img_dir, '{}_{}.png'.format(filename, sct)))
+    print("Image saved for {} x {}".format(model_name, sct))
+
+
+def box_visual(data: pd.DataFrame, task_name, img_dir):
+
+
+    plt.figure(figsize=(15, 10))
+    seaborn.set_theme(style="whitegrid")
+    sns_plot = seaborn.boxplot(data=data, x='sct', y='score', hue='model',  showfliers = False) \
+        .set_title(task_name, fontsize=30)
+    plt.legend(bbox_to_anchor=(-0.1, 1.05), loc='upper left', borderaxespad=0)
+    plt.xticks(rotation=20)
     # plt.show()
 
     # save graphs
     fig = sns_plot.get_figure()
-    filename = model_name.replace('/', '_')
-    fig.savefig(os.path.join(img_dir, '{}_{}.png'.format(filename, sct)))
-    print("Image saved for {} x {}".format(model_name, sct))
 
 
-img_dir = './'
-data_file = 'data.json'
-models = ['microsoft/codebert-base-mlm', 'microsoft/graphcodebert-base', 'dbernsohn/roberta-java']
-scts = ['renameTokens', 'permuteArgumentOrder', 'swapMultilineNoDeps', 'addExtraLogging']
+    fig.savefig(os.path.join(img_dir, '{}.png'.format(task_name)))
 
-data = None
-with open(data_file) as json_file:
-    data = json.load(json_file)
+    print("Image saved for {} task".format(task_name))
 
-for m in models:
-    for sct in scts:
-        visualization(data[m][sct]['after'], m, sct, img_dir)
+
+if __name__ == '__main__':
+    img_dir = './figs/'
+    data_file = './doc_completion.json'
+
+    models = ['microsoft/codebert-base-mlm', 'microsoft/graphcodebert-base', 'dbernsohn/roberta-java']
+    scts = ['renameTokens', 'permuteArgumentOrder', 'swapMultilineNoDeps', 'addExtraLogging']
+
+    # Read data
+    data = None
+    with open(data_file) as json_file:
+        data = json.load(json_file)
+
+    ############################### Histgram visualization ########################################
+    # for m in models:
+    #     for sct in scts:
+    #         hist_visual(data[m][sct], m, sct, img_dir)
+
+    ################################### Box Visualization ########################################
+    task_name = 'Document Synthesis'
+
+    df = {'model': [], 'sct': [], 'score': []}
+    for model in models:
+        for sct in scts:
+            bl = len(data[model][sct]['before'])
+            df['model'].extend([model + '(before)'] * bl)
+            df['sct'].extend([sct] * bl)
+            df['score'].extend(data[model][sct]['before'])
+            al = len(data[model][sct]['after'])
+            df['model'].extend([model + '(after)'] * al)
+            df['sct'].extend([sct] * al)
+            df['score'].extend(data[model][sct]['after'])
+
+
+    df = pd.DataFrame(data=df)
+    box_visual(df, task_name, img_dir)
