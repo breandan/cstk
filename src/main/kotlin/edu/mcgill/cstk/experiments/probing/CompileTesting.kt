e6f403711b829a918a6b4dfb736da49884eb04c3
breandan <bre@ndan.co>
2022-08-01 09:48:47 -0400

    parallelize compilation

--- src/main/kotlin/edu/mcgill/cstk/experiments/probing/CompileTesting.kt
+++ src/main/kotlin/edu/mcgill/cstk/experiments/probing/CompileTesting.kt
@@ -1,32 +1,38 @@
 package edu.mcgill.cstk.experiments.probing
 
-import ai.hypergraph.kaliningraph.parsing.dyckCheck
-import com.google.testing.compile.*
+import com.google.common.util.concurrent.AtomicLongMap
 import com.google.testing.compile.Compiler.javac
+import com.google.testing.compile.JavaFileObjects
 import edu.mcgill.cstk.disk.*
 import edu.mcgill.cstk.nlp.*
 import java.util.*
+import java.util.concurrent.atomic.LongAdder
 import javax.tools.*
+import kotlin.streams.*
 
 fun main() {
+  val map = AtomicLongMap.create<Model>()
+  val total = LongAdder()
+
   DATA_DIR
     .also { println("Evaluating $MODELS using compiler on $it...") }
     .allFilesRecursively().allMethods()
+    .asStream().parallel()
     .filter { it.first.startsWith("public static") && it.first.lines().size < 10 }
     .map { it.first.lines().joinToString("  ") }
     .filter { compilesWithoutSyntaxErrors(it) }
-    .runningFold(MODELS.associateWith { (0 to 0) }) { scores, code ->
-      MODELS.associateWith { model ->
+    .forEach { code ->
+      total.increment()
+      MODELS.forEach { model ->
         val prompt = code.constructPrompt(model.mask)
         val completion = model.complete(prompt, maxTokens = 1)
-        scores[model]!!.let { (n, d) ->
-          if (compilesWithoutSyntaxErrors(completion)) (n + 1) to (d+1)
-          else n to (d + 1)
-        }
+        if (compilesWithoutSyntaxErrors(completion)) map.incrementAndGet(model)
       }
+
+      val summary = map.asMap().entries
+        .map { (k, v) -> k to "$v/$total" }.joinToString("\n")
+      println("\nScores [model=(valid, total)]:\n$summary")
     }
-//    .filterIndexed { i, _ -> i % 10 == 0 }
-    .forEach { println("\nScores [model=(valid, total)]:\n${it.entries.joinToString("\n")}") }
 }
 
 private fun String.constructPrompt(mask: String) =
@@ -34,11 +40,10 @@ private fun String.constructPrompt(mask: String) =
 
 fun compilesWithoutSyntaxErrors(
   code: String,
-  file: JavaFileObject? =
-    JavaFileObjects.forSourceString(
-      "CompileTest",
-      """class CompileTest { $code }"""
-    ),
+  file: JavaFileObject? = JavaFileObjects.forSourceString(
+    "CompileTest",
+    """class CompileTest { $code }"""
+  ),
   syntaxErrors: List<Diagnostic<out JavaFileObject>> =
     javac().compile(file).errors()
       .filter { "cannot find symbol" !in it.getMessage(Locale.ENGLISH) }
