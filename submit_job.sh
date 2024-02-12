d6cfe6e74b553aeaa093c2033dd0727edb28e817
breandan <bre@ndan.co>
2024-02-12 11:23:09 -0500

    min free mem

--- submit_job.sh
+++ submit_job.sh
@@ -17,4 +17,4 @@ module load CCEnv StdEnv java/17.0.2
 # ./gradlew --offline completeCode | tee logfile.txt
 
 commit_message=$(git log -1 --pretty=format:"%s" | sed 's/ /_/g')
-java -Xmx170G -jar gym-fs-fat-1.0-SNAPSHOT.jar 2>&1 | tee /scratch/b/bengioy/breandan/log_${commit_message}.txt
+java -Xmx150G -jar gym-fs-fat-1.0-SNAPSHOT.jar 2>&1 | tee /scratch/b/bengioy/breandan/log_${commit_message}.txt
