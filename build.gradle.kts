3054b00c827fc7874dfdae70ce5398c871e057ff
breandan <bre@ndan.co>
2024-10-04 23:49:24 -0400

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   kotlin("jvm") version "2.0.0"
   id("com.github.ben-manes.versions") version "0.51.0"
   id("de.undercouch.download") version "5.6.0"
-  id("com.gradleup.shadow") version "8.3.2"
+  id("com.gradleup.shadow") version "8.3.3"
 }
 
 group = "com.github.breandan"
