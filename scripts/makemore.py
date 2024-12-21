0ac975b78334b411bfddc54ca08bb4d288f1f7af
breandan <bre@ndan.co>
2024-12-21 13:03:48 -0500

    train denoiser model

--- scripts/makemore.py
+++ scripts/makemore.py
@@ -361,23 +361,23 @@ if __name__ == '__main__':
     # parse command line args
     parser = argparse.ArgumentParser(description="Make More")
     # system/input/output
-    parser.add_argument('--input-file', '-i', type=str, default='unsupervised.txt', help="input file with things one per line")
+    parser.add_argument('--input-file', '-i', type=str, default='denoiser.txt', help="input file with things one per line")
     parser.add_argument('--work-dir', '-o', type=str, default='out', help="output working directory")
     parser.add_argument('--resume', action='store_true', help="when this flag is used, we will resume optimization from existing model in the workdir")
     parser.add_argument('--sample-only', action='store_true', help="just sample from the model and quit, don't train")
     parser.add_argument('--num-workers', '-n', type=int, default=4, help="number of data workers for both train/test")
     parser.add_argument('--max-steps', type=int, default=-1, help="max number of optimization steps to run for, or -1 for infinite.")
-    parser.add_argument('--device', type=str, default='cuda', help="device to use for compute, examples: cpu|cuda|cuda:2|mps")
+    parser.add_argument('--device', type=str, default='mps', help="device to use for compute, examples: cpu|cuda|cuda:2|mps")
     parser.add_argument('--seed', type=int, default=3407, help="seed")
     # sampling
     parser.add_argument('--top-k', type=int, default=-1, help="top-k for sampling, -1 means no top-k")
     # model
     parser.add_argument('--type', type=str, default='transformer', help="model class type to use, bigram|mlp|rnn|gru|bow|transformer")
-    parser.add_argument('--n-layer', type=int, default=16, help="number of layers")
-    parser.add_argument('--n-head', type=int, default=16, help="number of heads (in a transformer)")
-    parser.add_argument('--n-embd', type=int, default=512, help="number of feature channels in the model")
+    parser.add_argument('--n-layer', type=int, default=8, help="number of layers")
+    parser.add_argument('--n-head', type=int, default=8, help="number of heads (in a transformer)")
+    parser.add_argument('--n-embd', type=int, default=128, help="number of feature channels in the model")
     # optimization
-    parser.add_argument('--batch-size', '-b', type=int, default=256, help="batch size during optimization")
+    parser.add_argument('--batch-size', '-b', type=int, default=128, help="batch size during optimization")
     parser.add_argument('--learning-rate', '-l', type=float, default=5e-4, help="learning rate")
     parser.add_argument('--weight-decay', '-w', type=float, default=0.01, help="weight decay")
     args = parser.parse_args()
