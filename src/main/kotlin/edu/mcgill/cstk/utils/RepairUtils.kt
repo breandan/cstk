36f9729e1afa2c843bc84b7ac85eddf741444503
breandan <bre@ndan.co>
2025-02-14 20:33:15 -0500

    fix compile error

--- src/main/kotlin/edu/mcgill/cstk/utils/RepairUtils.kt
+++ src/main/kotlin/edu/mcgill/cstk/utils/RepairUtils.kt
@@ -29,14 +29,14 @@ fun Stream<Π2A<Σᐩ>>.minimizeFix(
   isValid: Σᐩ.() -> Boolean
 ): Stream<Π3A<Σᐩ>> =
   map { (broke, fixed) ->
-    minimizeFix(broke, tokenize, fixed, "", isValid) }
+    minimizeFixStr(broke, tokenize, fixed, "", isValid) }
 
 fun Sequence<Π2A<Σᐩ>>.minimizeFix(
   tokenize: Σᐩ.() -> List<Σᐩ>,
   isValid: Σᐩ.() -> Boolean
 ): Sequence<Π3A<Σᐩ>> =
   map { (broke, fixed) ->
-    minimizeFix(broke, tokenize, fixed, "", isValid) }
+    minimizeFixStr(broke, tokenize, fixed, "", isValid) }
 
 fun CFG.metrizedRepair(refStr: List<Σᐩ>, mc: MarkovChain<Σᐩ>): List<Repair> =
   solve(List(refStr.size + 3) { "_" }) {
